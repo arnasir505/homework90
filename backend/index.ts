8087e729d659dfd6b62032622c85d6cfc550773b
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-05-14 13:47:38 +0600

    Create express app with express-ws

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,22 @@
-ÿþ
+import express from 'express';
+import expressWs from 'express-ws';
+import cors from 'cors';
+
+const app = express();
+expressWs(app);
+
+const port = 8000;
+
+app.use(cors());
+
+const router = express.Router();
+
+router.ws('/paint', (ws, req) => {
+  console.log('client connected');
+});
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server started on ${port} port!`);
+});
